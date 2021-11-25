fb48733496dcca99820a009923778b17a3dcf7ac
misterInterlocked <kilian.schrenk@gmail.com>
2021-11-25 17:46:19 +0100

    updated breadcrumbs

--- src/common/Breadcrumb.tsx
+++ src/common/Breadcrumb.tsx
@@ -3,7 +3,7 @@ import Link from './Link';
 export default function Breadcrumb() {
     return  (
         <>
-            <Link href={'https//kilianschrenk.com'} target='_self'>kilianschrenk.com</Link>
+            <Link href={'https://kilianschrenk.com/blog'} target='_self'>kilianschrenk.com</Link>
             <span className={'text-light'}> ・ </span>
             <Link href={'#'} target='_self'>cv</Link>
         </>
